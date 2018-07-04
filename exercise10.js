e06d50c0ca456d4e4aa34e8c4862bc1228867c77
Tim <okidokisg@gmail.com>
2018-07-04 11:25:56 +0800

    f

--- exercise10.js
+++ exercise10.js
@@ -7,25 +7,49 @@
 //in the list, or undefined when there is no such element.
 //If you haven’t already, also write a recursive version of nth.
 
-//var list2 = {
-//	part : 1,
-//	node : {
-//		part : 2,
-//		node : {
-//			part : 3,
-//			node : null
+//var list = {
+//	value : 1,
+//	rest : {
+//		value : 2,
+//		rest : {
+//			value : 3,
+//			rest : null
 //		}
 //	}
 //};
 
-var list = {};
+var array = [1,33,544,5453];
 
-function arrayToList(arr) {
-	for (i = arr.length - 1; i >= 0; i--) {
-		list.part = arr[i];
-		list.node = list;
+console.log(array.map( x => x*1 ));
+	
+
+function arrayToList(array) {
+	let list = null;
+	for (let i = array.length - 1; i >= 0; i--) {
+		list = {value: array[i], rest: list};
 	}
 	return list;
 }
-
 console.log(arrayToList([ 1, 2, 3 ]));
+console.log(listToArray(arrayToList([10, 20, 30])));
+
+function listToArray(list) {
+	let array = [];
+	for (let node = list; node; node = node.rest) {
+		array.push(node.value);
+	}
+	return array;
+}
+
+function prepend(value, list) {
+	return {value, rest: list};
+}
+
+function nth(list, n) {
+	if (!list) return undefined;
+	else if (n == 0) return list.value;
+	else return nth(list.rest, n - 1);
+}
+
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayToList([10, 20, 30]), 1));
